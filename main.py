618e98e8ed19e427cb3de83f854692868161008f
Sigma-Ryden <math.divergent@gmail.com>
2024-11-27 02:59:00 +0200

    up

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from tkinter import ttk
 import random
 import time
 import threading
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+from tkinter import Toplevel, filedialog
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 # Ініціалізація головного вікна
 root = tk.Tk()
@@ -13,6 +19,32 @@ root.attributes('-toolwindow', True)  # Прибираємо системне м
 
 # Флаг для зупинки потоку
 stop_thread = threading.Event()
+
+audiofile_y = None
+audiofile_sr = None
+
+def plot_spectrogram(y, sr, n_fft=2048, hop_length=512, figsize=(10, 6)):
+    """
+    # Обчислення спектрограми (STFT)
+    n_fft # Оптимальний розмір вікна для більш точної частотної роздільної здатності
+    hop_length # Крок для перекриття вікон
+    """
+    D = librosa.stft(y, n_fft=n_fft, hop_length=hop_length, window=np.hanning(n_fft))
+    S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)
+    S_db += np.abs(S_db.min())
+
+    # Створення фігури для відображення спектрограми
+    fig, ax = plt.subplots(figsize=figsize)
+    img = librosa.display.specshow(S_db, sr=sr, hop_length=hop_length, x_axis='time', y_axis='log', cmap='viridis', ax=ax)
+    cbar = fig.colorbar(img, ax=ax, format='%+2.0f dB', ticks=np.linspace(S_db.min(), S_db.max(), 10), orientation='vertical')
+    cbar.set_label('Power (positive dB)')
+    cbar.ax.tick_params(labelsize=10)
+    ax.set_xlabel('Time (s)')
+    ax.set_xticks(np.linspace(0, S_db.shape[1] * hop_length / sr, 10))
+    ax.set_ylabel('Frequency (Hz)')
+    
+    plt.tight_layout()
+    return fig
 
 def truncate_text(text, max_length):
     """ Обрізає текст і додає '...' у разі перевищення максимального розміру. """
@@ -26,17 +58,22 @@ def load_mix():
         filetypes=[("WAV files", "*.wav")],
         title="Select a WAV file"
     )
+
     progress_bar['value'] = 0  # Скидання значення прогрес-бару в нуль
     if filepath:
         # Заблокувати кнопки Source і Show
         for button in source_buttons:
             button.config(state="disabled")
+
         global loaded_filename
         # Оновлення назви файлу в текстовому полі
         loaded_filename.set(truncate_text(filepath, 24))  # Обрізаємо до 24 символів
         root.title(f"SepSimTDA: {loaded_filename.get()}")  # Оновлення заголовку вікнаОновлення заголовку вікна
         # Розблокувати кнопку SeparateMix
         separate_button.config(state="normal")
+        
+        global audiofile_y, audiofile_sr
+        audiofile_y, audiofile_sr = librosa.load(filepath, sr=None)
 
 
 def save_source(index):
@@ -51,8 +88,27 @@ def save_source(index):
         print(f"File saved to: {save_path}")
 
 def spectrogram(index):
+    global audiofile_y, audiofile_sr
+
     """ Функція, що викликається при натисканні кнопки Spectrogram1. """
     print(f"Spectrogram called with index: {index}")
+
+    fig = plot_spectrogram(audiofile_y, audiofile_sr, figsize=(8, 4))
+
+    # Створення нового вікна для відображення спектрограми
+    spectrogram_window = Toplevel()
+    spectrogram_window.title(f"SepSimTDA: Voice{index}")
+    spectrogram_window.resizable(False, False)
+    spectrogram_window.attributes('-toolwindow', True)
+
+    # Відображення в новому вікні через tkinter
+    canvas = FigureCanvasTkAgg(fig, master=spectrogram_window)
+    canvas.get_tk_widget().pack()
+    canvas.draw()
+
+
+def audio_spectrogram():
+    spectrogram(0) # TODO: temp
 
 def separate_mix():
     """ Функція для створення випадкової кількості кнопок з симуляцією затримки. """
@@ -88,6 +144,7 @@ def separate_mix():
     threading.Thread(target=process_separation, daemon=True).start()
 
 
+
 # Список для збереження кнопок Source
 source_buttons = []
 
@@ -103,7 +160,7 @@ load_button = tk.Button(button_frame, text="LoadAudio", command=load_mix)
 load_button.pack(side="left", padx=5)
 
 # Створення кнопки MixSpectrogram
-mix_spectrogram_button = tk.Button(button_frame, text="ShowSpectrogram", command=lambda: spectrogram(0))
+mix_spectrogram_button = tk.Button(button_frame, text="ShowSpectrogram", command=audio_spectrogram)
 mix_spectrogram_button.pack(side="left", padx=5)
 
 # Кнопка SeparateMix і progress_bar в третьому рядку
@@ -111,7 +168,7 @@ separate_frame = tk.Frame(root)
 separate_frame.pack(side="top", fill="x", padx=10, pady=10)
 
 # Створення кнопки SeparateMix (спочатку заблокована)
-separate_button = tk.Button(separate_frame, text="SeparateAudio", command=separate_mix, state="disabled")
+separate_button = tk.Button(separate_frame, text="Separation", command=separate_mix, state="disabled")
 separate_button.pack(side="left", padx=5)
 
 # Прогрес-бар для відображення процесу розділення (завжди показаний праворуч від кнопки SeparateMix)
@@ -141,74 +198,3 @@ root.protocol("WM_DELETE_WINDOW", on_closing)
 
 # Запуск головного циклу програми
 root.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-# import librosa
-# import librosa.display
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import tkinter as tk
-# from tkinter import Toplevel, filedialog
-# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-
-# def plot_spectrogram(y, sr, n_fft=2048, hop_length = 512, figsize=(10, 6)):
-#     """
-#     # Обчислення спектрограми (STFT)
-#     n_fft # Оптимальний розмір вікна для більш точної частотної роздільної здатності
-#     hop_length # Крок для перекриття вікон
-#     """
-#     D = librosa.stft(y, n_fft=n_fft, hop_length=hop_length, window=np.hanning(n_fft))
-#     S_db = librosa.amplitude_to_db(np.abs(D), ref=np.max)
-#     S_db += np.abs(S_db.min())
-
-#     # Створення фігури для відображення спектрограми
-#     fig, ax = plt.subplots(figsize=figsize)
-#     img = librosa.display.specshow(S_db, sr=sr, hop_length=hop_length, x_axis='time', y_axis='log', cmap='viridis', ax=ax)
-#     cbar = fig.colorbar(img, ax=ax, format='%+2.0f dB', ticks=np.linspace(S_db.min(), S_db.max(), 10), orientation='vertical')
-#     cbar.set_label('Power (positive dB)')
-#     cbar.ax.tick_params(labelsize=10)
-#     ax.set_xlabel('Time (s)')
-#     ax.set_xticks(np.linspace(0, S_db.shape[1] * hop_length / sr, 10))
-#     ax.set_ylabel('Frequency (Hz)')
-    
-#     plt.tight_layout()
-#     return fig
-
-# # Створення графічного інтерфейсу користувача
-# root = tk.Tk()
-# root.title("Spectrogram Viewer")
-# root.geometry("300x150")
-
-# def open_file_and_plot():
-#     # Вибір аудіофайлу через діалогове вікно
-#     audio_path = filedialog.askopenfilename(filetypes=[("Audio Files", "*.wav")])
-#     if audio_path:
-#         y, sr = librosa.load(audio_path, sr=None)
-#         fig = plot_spectrogram(y, sr, figsize=(8, 4))
-        
-#         # Створення нового вікна для відображення спектрограми
-#         spectrogram_window = Toplevel()
-#         spectrogram_window.title("SepSimTDA-Source0")
-#         spectrogram_window.resizable(False, False)
-#         spectrogram_window.attributes('-toolwindow', True)
-
-#         # Відображення в новому вікні через tkinter
-#         canvas = FigureCanvasTkAgg(fig, master=spectrogram_window)
-#         canvas.get_tk_widget().pack()
-#         canvas.draw()
-
-# # Додавання кнопки для відображення спектрограми
-# button = tk.Button(root, text="Open Audio File and Plot Spectrogram", command=open_file_and_plot)
-# button.pack(expand=True)
-
-# root.protocol("WM_DELETE_WINDOW", root.quit)
-# root.mainloop()
